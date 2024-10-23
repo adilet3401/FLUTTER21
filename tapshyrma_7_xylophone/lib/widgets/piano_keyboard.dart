50c4fc79d75c4306c95fb41d5500347bfcd5d690
Adilet <adilet.dildeev@gmail.com>
2024-10-23 21:24:17 +0600

    x

--- tapshyrma_7_xylophone/lib/widgets/piano_keyboard.dart
+++ tapshyrma_7_xylophone/lib/widgets/piano_keyboard.dart
@@ -1,30 +1,26 @@
-import 'package:audioplayers/audioplayers.dart';
+// import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 
 class PianoKeyboard extends StatelessWidget {
   final Color colors;
-  final String soundName;
+  // final String soundName;
+  final void Function()? onTap;
 
-  const PianoKeyboard(
-      {super.key, required this.colors, required this.soundName});
+  const PianoKeyboard({
+    super.key,
+    required this.colors,
+    this.onTap,
+  });
 
   @override
   Widget build(BuildContext context) {
-    final AudioPlayer player = AudioPlayer();
-    // final AudioCache cache = AudioCache(prefix: 'assets/sounds/');
-
-    return Expanded(
-      child: GestureDetector(
-        onTap: () async {
-          await player.setSource(AssetSource('assets/sounds/$soundName.wav'));
-          player.resume();
-        },
-        child: Container(
-          width: double.infinity,
-          height: 91,
-          decoration: BoxDecoration(
-            color: colors,
-          ),
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        width: double.infinity,
+        height: 104,
+        decoration: BoxDecoration(
+          color: colors,
         ),
       ),
     );
--- tapshyrma_7_xylophone/lib/xylophone_page.dart
+++ tapshyrma_7_xylophone/lib/xylophone_page.dart
@@ -1,9 +1,11 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 import 'package:tapshyrma_7_xylophone/widgets/piano_keyboard.dart';
 // import 'package:flutter/widgets.dart';
 
 class XylophoneApp extends StatelessWidget {
   const XylophoneApp({super.key});
+  // AudioPlayer audio = AudioPlayer();
 
   @override
   Widget build(BuildContext context) {
@@ -14,8 +16,15 @@ class XylophoneApp extends StatelessWidget {
   }
 }
 
-class Xylophone extends StatelessWidget {
+class Xylophone extends StatefulWidget {
   const Xylophone({super.key});
+
+  @override
+  State<Xylophone> createState() => _XylophoneState();
+}
+
+class _XylophoneState extends State<Xylophone> {
+  AudioPlayer audio = AudioPlayer();
 
   @override
   Widget build(BuildContext context) {
@@ -23,22 +32,77 @@ class Xylophone extends StatelessWidget {
       appBar: myAppBar(),
       body: Column(
         children: [
-          const PianoKeyboard(colors: Color(0xffef443a), soundName: 'note1'),
-          const PianoKeyboard(colors: Color(0xffF99700), soundName: 'note2'),
-          const PianoKeyboard(colors: Color(0xffFFE93B), soundName: 'note3'),
-          const PianoKeyboard(colors: Color(0xff4CB050), soundName: 'note4'),
-          const PianoKeyboard(colors: Color(0xff2E968C), soundName: 'note5'),
-          const PianoKeyboard(colors: Color(0xff2996F5), soundName: 'note6'),
-          const PianoKeyboard(colors: Color(0xff9B28B1), soundName: 'note7'),
-          Expanded(
-            child: Container(
-              width: double.infinity,
-              height: 70,
-              decoration: const BoxDecoration(
-                color: Color(0xff363636),
-              ),
+          PianoKeyboard(
+            colors: const Color(0xffEF443A),
+            onTap: () async {
+              await audio.play(
+                AssetSource('do.wav'),
+              );
+            },
+          ),
+          PianoKeyboard(
+            colors: const Color(0xffF99700),
+            onTap: () {},
+          ),
+          PianoKeyboard(
+            colors: const Color(0xffFFE93B),
+            onTap: () {},
+          ),
+          PianoKeyboard(
+            colors: const Color(0xff4CB050),
+            onTap: () {},
+          ),
+          PianoKeyboard(
+            colors: const Color(0xff2E968C),
+            onTap: () {},
+          ),
+          PianoKeyboard(
+            colors: const Color(0xff2996F5),
+            onTap: () {},
+          ),
+          PianoKeyboard(
+            colors: const Color(0xff9B28B1),
+            onTap: () {},
+          ),
+          // Expanded(
+          // child: Container(
+          //   width: double.infinity,
+          //   height: 70,
+          //   decoration: const BoxDecoration(
+          //     color: Color(0xff363636),
+          //   ),
+          // ),
+          // ),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: const Color(0xff363636),
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            backgroundColor: Color(0xff363636),
+            icon: Icon(
+              Icons.home,
             ),
-          )
+            label: 'home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.settings,
+            ),
+            label: 'settings',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.search_rounded,
+            ),
+            label: 'home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.person,
+            ),
+            label: 'home',
+          ),
         ],
       ),
     );
